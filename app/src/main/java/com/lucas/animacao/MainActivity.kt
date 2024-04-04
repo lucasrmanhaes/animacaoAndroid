9c14d5588836452f6be1613b27c3983cf1f55144
lucas <ramosmanhaes@outlook.com>
2024-04-04 12:27:00 -0300

    Criando estrutura do projeto

---
+++ app/src/main/java/com/lucas/animacao/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.lucas.animacao
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucas.animacao.ui.theme.AnimacaoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimacaoTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
